4b853102b39a4e9c4f373f72101052e5fb7d0375
Muthuganesan N G <96805495+Muthuganesan2003@users.noreply.github.com>
2025-07-17 21:03:28 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (93.31%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -10,8 +10,8 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        if(head==null)return null;
-        if(head.next == null)return head;
+        
+        if(head==null || head.next == null|| left == right)return head;
         ListNode mov = head;
         ListNode prev = null;
         int count = 0;
